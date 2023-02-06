7d78ad576211fa2ea3a3861b6b4669fe2fd31ca6
JustNU <ivancool108@gmail.com>
2023-02-06 06:02:23 +0300

    Move locale and bot code outside of voice function

--- src/mod.js
+++ src/mod.js
@@ -6,20 +6,55 @@ class Mod
 	{
 		const logger = container.resolve("WinstonLogger");
 		const database = container.resolve("DatabaseServer").getTables();
+		const VFS = container.resolve("VFS");
+		const enLocale = require(`../db/locales/en.json`);
+		const config = require("../config/config.json");
 		
-		Mod.makeVoice("Stalker_Bandit_1", logger, database);
-		Mod.makeVoice("Monolith_3", logger, database);
-		Mod.makeVoice("Freedom_1", logger, database);
-		Mod.makeVoice("Stalker_2", logger, database);
-		Mod.makeVoice("ClearSky_1", logger, database);
-		Mod.makeVoice("Duty_1", logger, database);
-	};
+		Mod.makeVoice("Stalker_Bandit_1", database);
+		Mod.makeVoice("Monolith_3", database);
+		Mod.makeVoice("Freedom_1", database);
+		Mod.makeVoice("Stalker_2", database);
+		Mod.makeVoice("ClearSky_1", database);
+		Mod.makeVoice("Duty_1", database);
+		
+		// handle locale
+		for (const localeID in database.locales.global) {
+			// en placeholder
+			for (const itemId in enLocale) {
+				database.locales.global[localeID][`${itemId} Name`] = enLocale[itemId];
+			}
+			
+			if (VFS.exists(`StalkerVoices/locales/${localeID}.json`) && localeID != "en") {
+				const actualLocale = require(`../locales/${localeID}.json`);
+				
+				for (const itemId in actualLocale) {
+					database.locales.global[localeID][`${itemId} Name`] = actualLocale[itemId];
+				}
+			}
+		}
+		
+		// add to bots
+		for (const botInConfig in config.AddVoicesToBots) {
+			for (const botInDb in database.bots.types) {
+				if (botInConfig === botInDb) {
+					if (config.AddVoicesToBots[botInConfig].AddFollowingVoices.length != 0) {
+						if (config.AddVoicesToBots[botInConfig].ReplaceDefaultOnes) {
+							database.bots.types[botInDb].appearance.voice = [];
+						}
+						
+						for (const voiceIndex in config.AddVoicesToBots[botInConfig].AddFollowingVoices) {
+							database.bots.types[botInDb].appearance.voice.push(config.AddVoicesToBots[botInConfig].AddFollowingVoices[voiceIndex])
+						}
+					}
+				}
+			}
+		}
+	}
 	
-	static makeVoice(voiceId, logger, database)
+	static makeVoice(voiceId, database)
 	{
 		// constants
 		const config = require("../config/config.json");
-		let locale = require(`../db/locales/en.json`);
 		
 		// make da voice
 		const newVoice = {
@@ -38,32 +73,8 @@ class Mod
 		
 		// add new voice to customization and character
 		database.templates.customization[voiceId] = newVoice;
-		database.templates.character.push(voiceId);
-		
-		// locale
-		for (const localeID in database.locales.global)
-        {
-			database.locales.global[localeID][`${voiceId} Name`] = locale[voiceId];
-        };
-		
-		// add to bots
-		for (const botInConfig in config.AddVoicesToBots) {
-			for (const botInDb in database.bots.types) {
-				if (botInConfig === botInDb) {
-					if (config.AddVoicesToBots[botInConfig].AddFollowingVoices.length != 0) {
-						if (config.AddVoicesToBots[botInConfig].ReplaceDefaultOnes) {
-							database.bots.types[botInDb].appearance.voice = [];
-						};
-						
-						for (const voiceIndex in config.AddVoicesToBots[botInConfig].AddFollowingVoices) {
-							database.bots.types[botInDb].appearance.voice.push(config.AddVoicesToBots[botInConfig].AddFollowingVoices[voiceIndex])
-						};
-					};
-				};
-			};
-		};
-		
-	};
-};
+		database.templates.character.push(voiceId);	
+	}
+}
 
 module.exports = { mod: new Mod() }
